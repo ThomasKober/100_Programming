43655090264760a1f0564326b67f46df8a88fca4
ThomasKober <86011122+ThomasKober@users.noreply.github.com>
2025-08-17 15:21:24 +0200

    update
    created project folder structure

---
+++ Visual Studio/BLE-Interface/BLE-Interface/ViewModels/DeviceScanViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLE_Interface.ViewModels
+{
+    internal class DeviceScanViewModel
+    {
+    }
+}
